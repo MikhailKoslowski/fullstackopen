39fddf353a0d5550ad9242b2a48fade4bfd12b08
Mikhail A Koslowski <mikhail.koslowski@gmail.com>
2020-09-26 21:29:34 -0300

    Exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,12 @@ const totalLikes = (blogs) => {
  return blogs.reduce( (acc, b) => acc + b.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce( (a,b) => a.likes > b.likes ? a : b )
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
