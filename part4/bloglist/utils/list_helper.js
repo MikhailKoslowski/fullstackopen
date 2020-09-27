368f907707db9346637e2141fc232c42fb3aeeda
Mikhail A Koslowski <mikhail.koslowski@gmail.com>
2020-09-26 23:02:58 -0300

    Exercises 4.6 and 4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,3 @@
-const { mapReduce } = require("../models/blog")
-
 const dummy = (blogs) => {
   return 1
 }
@@ -12,8 +10,37 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce( (a,b) => a.likes > b.likes ? a : b )
 }
 
+const mostBlogs = (blogs) => {
+    const authorsBlogs = []
+    blogs.forEach(blog => {
+      const index = authorsBlogs.findIndex( b => b.author === blog.author)
+      if (index >= 0) {
+        authorsBlogs[index].blogs = authorsBlogs[index].blogs + 1
+      } else {
+        authorsBlogs.push({author:blog.author, blogs:1})
+      }
+    })
+    
+  return authorsBlogs.reduce( (a,b) => a.blogs > b.blogs ? a : b , {author:"", blogs:0})
+}
+
+const mostLikes = (blogs) => {
+  const authorsBlogs = []
+    blogs.forEach(blog => {
+      const index = authorsBlogs.findIndex( b => b.author === blog.author)
+      if (index >= 0) {
+        authorsBlogs[index].likes = authorsBlogs[index].likes + blog.likes
+      } else {
+        authorsBlogs.push({author:blog.author, likes:blog.likes})
+      }
+    })
+    
+  return authorsBlogs.reduce( (a,b) => a.likes > b.likes ? a : b , {author:"", likes:0})
+}
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
