9d8e3ff33944b8fe0cfdbbe547caeb465fb07f93
Mikhail <mikhail.koslowsk@gmail.com>
2020-10-04 22:08:54 -0300

    Exercise 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -7,32 +7,64 @@ const Blog = require('../models/blog')
 const helper = require('./test_helper')
 const { initialBlogs } = require('./test_helper')
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  for (let blog of helper.initialBlogs) {
-    let blogObj = new Blog(blog)
-    await blogObj.save()
-  }
-})
+describe('blog api', () => {
 
-test('blog api get all', async () => {
-  const response = await api.get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-  
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    for (let blog of helper.initialBlogs) {
+      let blogObj = new Blog(blog)
+      await blogObj.save()
+    }
+  })
 
-test('unique identifier is id', async () => {
-  const response = await api.get('/api/blogs')
-  
-  for (let blog of response.body )
-  {
-    expect(blog.id).toBeDefined()
-  }
-})
+  test('get all', async () => {
+    const response = await api.get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    
+      expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 
-afterAll(() => {
-  mongoose.connection.close()
-  logger.info('disconnected from MongoDB')
+  test('unique identifier is id', async () => {
+    const response = await api.get('/api/blogs')
+    
+    for (let blog of response.body )
+    {
+      expect(blog.id).toBeDefined()
+    }
+  })
+
+  test('post works', async () => {
+    const blog = {
+      title: "Test Blog",
+      author: "Mikhail",
+      url: "http://localhost",
+      likes: 0
+    }
+    
+    // get blogs length
+    let response = await api.get('/api/blogs')
+    const beforeLen = response.body.length
+    
+    // post
+    response = await api.post('/api/blogs')
+      .send(blog)
+      .expect(201)
+
+    // except for the id, they should be equal.
+    blog.id = response.body.id
+    expect(response.body).toEqual(blog)
+
+    // get new len.
+    response = await api.get('/api/blogs')
+    const afterLen = response.body.length
+
+    expect(afterLen).toEqual(beforeLen + 1)
+  })
+
+  afterAll(() => {
+    mongoose.connection.close()
+    logger.info('disconnected from MongoDB')
+  })
+
 })
